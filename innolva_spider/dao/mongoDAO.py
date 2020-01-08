64d9655a8db5ec48627e62734d0417448b9a872c
beena95 <lorenzo.bin95@gmail.com>
2020-01-08 17:10:47 +0100

    minor changes to mongoDAO class

--- innolva_spider/dao/mongoDAO.py
+++ innolva_spider/dao/mongoDAO.py
@@ -33,23 +33,28 @@ class MongoDAO:
                 coll.insert(o, check_keys=False)
 
     def update_by_id(self, collection, id, key_values_dict):
+        """this function updates the document that match the input id"""
         coll = self.get_coll(collection)
         if self.get_by_id(collection, id):
             coll.find_one_and_update({"_id": ObjectId(id)}, {"$set": key_values_dict}, upsert=True)
 
     def update_by_condition_dict(self, collection, conditiondict, updatedict):
+        """this function updates the document that match the condition"""
         coll = self.get_coll(collection)
         coll.update_one(conditiondict, {"$set": updatedict}, upsert=True)
 
     def delete_by_id(self, collection, id):
+        """this function delete the document that has input id"""
         coll = self.get_coll(collection)
         coll.remove({"_id": ObjectId(id)})
 
     def delete_by_condition_dict(self, collection, conditiondict):
+        """this function delete the document that the condition"""
         coll = self.get_coll(collection)
         coll.remove(conditiondict, True)
 
     def all(self, collection, start: int = 0, rows: int = None, selection=None):
+        """return the whole collection"""
         coll = self.get_coll(collection)
         if rows:
             for el in coll.find(None, selection).skip(start).limit(rows):
@@ -61,6 +66,7 @@ class MongoDAO:
                 yield el
 
     def links_list(self, collection, start: int = 0):
+        """get the links throught a set"""
         my_set = set()
         coll = self.get_coll(collection)
         for el in coll.find().skip(start):
@@ -69,6 +75,7 @@ class MongoDAO:
         return my_set
 
     def get_by_id(self, collection, id):
+        """get the document that match the id"""
         coll = self.get_coll(collection)
         el = coll.find_one({"_id": ObjectId(id)})
         if not el:
@@ -77,6 +84,7 @@ class MongoDAO:
         return el
 
     def get_by_condition_dict(self, collection, conditiondict):
+        """get the document based on id"""
         coll = self.get_coll(collection)
         el = coll.find_one(conditiondict)
         if not el:
@@ -84,13 +92,16 @@ class MongoDAO:
         return el
 
     def collections(self):
+        """get all the collections"""
         return self.client.get_database(self.db).collection_names()
 
     def drop(self, collection):
+        """delete a single collection"""
         coll = self.get_coll(collection)
         coll.drop()
 
     def get_coll(self, collection):
+        """get a collection"""
         return self.client.get_database(self.db).get_collection(collection)
 
     def sample(self, collection, n):
@@ -100,12 +111,14 @@ class MongoDAO:
             yield el
 
     def query(self, collection, where, select=None):
+        """with this function you can do a query similar to sql database"""
         coll = self.get_coll(collection)
         for el in coll.find(where, select):
             el["_id"] = str(el["_id"])
             yield el
 
     def copy(self, collection1, collection2):
+        """copy everything inside the first collection inside the second collection"""
         coll1 = self.get_coll(collection1)
         coll2 = self.get_coll(collection2)
         coll2.remove()
@@ -113,12 +126,15 @@ class MongoDAO:
             coll2.insert(el)
 
     def count(self, collection):
+        """count the documents inside the collection"""
         return self.get_coll(collection).count()
 
     def get_last(self, collection, q: {} = None):
+        """get the last document added to collection"""
         coll = self.get_coll(collection)
         return coll.find_one(sort=[('_id', DESCENDING)])
 
     def clear_collection(self, collection):
+        """remove every document inside a collection"""
         coll = self.get_coll(collection)
         coll.remove({})
