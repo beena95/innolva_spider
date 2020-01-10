298b8cad628583ca3ffc35e61e37524d72f7c9a6
beena95 <lorenzo.bin95@gmail.com>
2020-01-10 16:33:14 +0100

    changes to articleToDB

--- innolva_spider/dao/ArticleToDB.py
+++ innolva_spider/dao/ArticleToDB.py
@@ -10,11 +10,7 @@ class ArticleToDB(MongoDAO):
         super().__init__(host, db)
 
     def save(self, obj, collection: str):
-<<<<<<< HEAD
-        """save a link or an article into a collection"""
-=======
         """save a single article or a single string"""
->>>>>>> d91b09296389306ded1a8b4961c922b28aa9fe22
         coll = self.get_coll(collection)
         if type(obj) == str:
             coll.insert_one({"Link": obj})
@@ -29,11 +25,7 @@ class ArticleToDB(MongoDAO):
             coll.insert_one(dict)
 
     def save_list(self, obj, collection: str):
-<<<<<<< HEAD
-        """save an article list or a string list inside a collection"""
-=======
         """save multiple obj or str inside a list"""
->>>>>>> d91b09296389306ded1a8b4961c922b28aa9fe22
         coll = self.get_coll(collection)
         for link in obj:
             if type(link) == str:
@@ -49,11 +41,7 @@ class ArticleToDB(MongoDAO):
                 coll.insert_one(dict)
 
     def update_multiple_by_condition_dict(self, collection: str, condition_dict: dict, update_dict: dict):
-<<<<<<< HEAD
-        """update more documents that match a condition"""
-=======
         """update multiple documents that match a condition"""
->>>>>>> d91b09296389306ded1a8b4961c922b28aa9fe22
         coll = self.get_coll(collection)
         coll.update_many(condition_dict, {"$set": update_dict}, upsert=True)
 
