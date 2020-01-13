bb13ece0e2951128cbcce1254a5843be9f078dfd
beena95 <lorenzo.bin95@gmail.com>
2020-01-13 18:25:35 +0100

    changes to MongoDAO

--- innolva_spider/dao/mongoDAO.py
+++ innolva_spider/dao/mongoDAO.py
@@ -1,16 +1,14 @@
 from bson import ObjectId
 from pymongo import MongoClient, DESCENDING
 
-
 class MongoDAO:
-
-    def __init__(self, host, db):
+    def __init__(self, host, db, port=None):
         self.db = db
         self.host = host
-        self.client = MongoClient(host=self.host)
-
+        self.port = port
+        self.client = MongoClient(host=self.host, port=self.port)
     def save(self, collection, obj):
-        coll = self.get_coll(collection)
+        coll = self.getcoll(collection)
         if not type(obj) == list:
             obj = [obj]
         obj_to_insert = [o for o in obj if "_id" not in o]
@@ -30,30 +28,20 @@ class MongoDAO:
                     coll.insert(o, check_keys=False)
             except:
                 coll.insert(o, check_keys=False)
-
-    def update_by_id(self, collection: str, id: str, key_values_dict: dict):
-        """this function updates the document that match the input id"""
-        coll = self.get_coll(collection)
+    def update_by_id(self, collection, id, key_values_dict):
+        coll = self.getcoll(collection)
         coll.find_one_and_update({"_id": ObjectId(id)}, {"$set": key_values_dict}, upsert=True)
-
-    def update_by_condition_dict(self, collection, condition_dict: dict, update_dict: dict):
-        """this function updates the document that match the condition"""
-        coll = self.get_coll(collection)
-        coll.update_one(condition_dict, {"$set": update_dict}, upsert=True)
-
-    def delete_by_id(self, collection: str, id: str):
-        """this function delete the document that match input id"""
-        coll = self.get_coll(collection)
+    def update_by_conditiondict(self, collection, conditiondict, updatedict):
+        coll = self.getcoll(collection)
+        coll.update_one(conditiondict, {"$set": updatedict}, upsert=True)
+    def delete_by_id(self, collection, id):
+        coll = self.getcoll(collection)
         coll.remove({"_id": ObjectId(id)})
-
-    def delete_by_condition_dict(self, collection: str, condition_dict: dict):
-        """this function delete the first document that match the condition"""
-        coll = self.get_coll(collection)
-        coll.remove(condition_dict, True)
-
-    def all(self, collection: str, start: int = 0, rows: int = None, selection=None):
-        """return the whole collection"""
-        coll = self.get_coll(collection)
+    def delete_by_conditiondict(self, collection, conditiondict):
+        coll = self.getcoll(collection)
+        coll.remove(conditiondict, True)
+    def all(self, collection, start: int = 0, rows: int = None, selection=None):
+        coll = self.getcoll(collection)
         if rows:
             for el in coll.find(None, selection).skip(start).limit(rows):
                 el["_id"] = str(el["_id"])
@@ -62,67 +50,44 @@ class MongoDAO:
             for el in coll.find().skip(start):
                 el["_id"] = str(el["_id"])
                 yield el
-
-    def get_by_id(self, collection: str, id: str):
-        """get the document that match the id"""
-        coll = self.get_coll(collection)
+    def getbyid(self, collection, id):
+        coll = self.getcoll(collection)
         el = coll.find_one({"_id": ObjectId(id)})
         if not el:
             raise Exception("Element with id " + str(id) + " not found")
         el["_id"] = str(el["_id"])
         return el
-
-    def get_by_condition_dict(self, collection: str, condition_dict: dict):
-        """get the document based on id"""
-        coll = self.get_coll(collection)
-        el = coll.find_one(condition_dict)
+    def get_by_conditiondict(self, collection, conditiondict):
+        coll = self.getcoll(collection)
+        el = coll.find_one(conditiondict)
         if not el:
             raise Exception("Element not found")
         return el
-
     def collections(self):
-        """get all the collections"""
         return self.client.get_database(self.db).collection_names()
-
-    def drop(self, collection: str):
-        """delete a single collection"""
-        coll = self.get_coll(collection)
+    def drop(self, collection):
+        coll = self.getcoll(collection)
         coll.drop()
-
-    def get_coll(self, collection: str):
-        """get a collection"""
+    def getcoll(self, collection):
         return self.client.get_database(self.db).get_collection(collection)
-
-    def sample(self, collection: str, n):
-        coll = self.get_coll(collection)
+    def sample(self, collection, n):
+        coll = self.getcoll(collection)
         for el in coll.aggregate([{"$sample": {"size": n}}]):
             el["_id"] = str(el["_id"])
             yield el
-
     def query(self, collection, q):
-        coll = self.get_coll(collection)
+        coll = self.getcoll(collection)
         for el in coll.find(q):
             el["_id"] = str(el["_id"])
             yield el
-
-    def copy(self, collection1: str, collection2: str):
-        """copy everything inside the first collection inside the second collection"""
-        coll1 = self.get_coll(collection1)
-        coll2 = self.get_coll(collection2)
+    def copy(self, collection1, collection2):
+        coll1 = self.getcoll(collection1)
+        coll2 = self.getcoll(collection2)
         coll2.remove()
         for el in coll1.find():
             coll2.insert(el)
-
-    def count(self, collection: str):
-        """count the documents inside the collection"""
-        return self.get_coll(collection).count()
-
-    def get_last(self, collection: str, q: {} = None):
-        """get the last document added to collection"""
-        coll = self.get_coll(collection)
+    def count(self, collection):
+        return self.getcoll(collection).count()
+    def get_last(self, collection, q: {} = None):
+        coll = self.getcoll(collection)
         return coll.find_one(sort=[('_id', DESCENDING)])
-
-    def clear_collection(self, collection: str):
-        """remove every document inside a collection"""
-        coll = self.get_coll(collection)
-        coll.remove({})
